5cfcd303b8878cd3dd291dc1f105de896126237e
Cheater0809 <tom.gemeinder@gmail.com>
2025-09-24 18:19:53 +0200

    started on main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+def get_book_text(filepath: str): 
+    with open(filepath) as f:
+        return f.read()
+    
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+    
+main()
