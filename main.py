bd20e4c4e5e6f0d111ad93220b4c3ea650fdcc1c
Cheater0809 <tom.gemeinder@gmail.com>
2025-09-24 18:34:46 +0200

    moved count_words to stats.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+from stats import count_words
+
 def get_book_text(filepath: str) -> str: 
     with open(filepath) as f:
         return f.read()
-    
-def count_words(text: str) -> int:
-    words = text.split()
-    return len(words)
-    
     
 def main() -> None:
     print (f"Found {count_words(get_book_text("books/frankenstein.txt"))} total words")
