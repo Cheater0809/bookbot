59facd7d58ba48ccc765f6ade54e827388564bde
Cheater0809 <tom.gemeinder@gmail.com>
2025-09-24 22:30:54 +0200

    switched to CLI bookpath via sys.argv

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from stats import *
+import sys
 
+if len(sys.argv) !=2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+    
 def get_book_text(filepath: str) -> str: 
     with open(filepath) as f:
         return f.read()
     
 def main() -> None:
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     wordCount = count_words(book_text)
     sorted_charCounts = sort_dict(count_characters(book_text))
